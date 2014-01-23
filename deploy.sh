e1758da4c79abfc5fd0f6c8468f26b9de76c8970
Anton Grbin <anton.grbin@fer.hr>
2014-01-23 22:04:52 +0100

    deploy script updated.

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 # in ../hwop-engine/.
 # you can test your engine with ../hwop-engine/hwop test
 
+# if error occurs stop.
+set -e
+
 ../hwop-engine/hwop build --src=wiki --dst=/tmp/hwop-output
-git checkout -b gh-pages
+git checkout gh-pages
 rm -rf *
 cp -r /tmp/hwop-output/* .
 git add --all
-git commit -a -m 'gh-pages updated.'
-git push
+git commit -m 'gh-pages updated.'
+git push origin HEAD:gh-pages
 git checkout master
+rm -rf /tmp/hwop-output
