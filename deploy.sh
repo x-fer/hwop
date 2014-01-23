eee3233926c6d4e4dedffbe19bbb0400146b7c47
Anton Grbin <anton.grbin@fer.hr>
2014-01-23 22:16:20 +0100

    testing deploy script more.

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,14 @@
 set -e
 
 ../hwop-engine/hwop build --src=wiki --dst=/tmp/hwop-output
+
+git stash
 git checkout gh-pages
 rm -rf *
 cp -r /tmp/hwop-output/* .
 git add --all
-git commit -m 'gh-pages updated.'
+git commit --allow-empty --message='gh-pages updated.'
 git push origin HEAD:gh-pages
 git checkout master
 rm -rf /tmp/hwop-output
+git stash pop
