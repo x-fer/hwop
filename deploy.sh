fcc9f6924b192d7543467bf0b33ea360f914e33e
Anton Grbin <anton.grbin@fer.hr>
2014-01-24 13:27:06 +0100

    more changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,17 @@
 
 outdir=../__hwop_html
 
+[ -f deploy.sh ] || ( echo "must be in root hwop dir."; exit 1; )
+
 ../hwop-engine/hwop build --src=wiki --dst=$outdir
 
-git stash
+git stash > /dev/null
 
-  git checkout gh-pages
-  git rm -r *
+  git checkout gh-pages &> /dev/null
+  rm -rf * > /dev/null
   cp -r $outdir/* .
-  git add --all
+  git add --all > /dev/null
   git commit --message='gh-pages updated.' && git push origin HEAD:gh-pages
-  git checkout master
+  git checkout master &> /dev/null
 
-git stash pop
+git stash pop > /dev/null
